5c693fe445da166e4d97ef702b6937b922e964f7
Yuliya <ypolis@softserveinc.com>
2022-02-08 17:01:04 +0200

    Added for loop for automatization questions and if else statements

--- js/script.js
+++ js/script.js
@@ -7,14 +7,32 @@ const personalMovieDB = {
     actors: {},
     genres: [],
     privat: false
+
 };
 
-
-for(let i = 0; i < 2; i++) {
-    let firstQuestion = prompt("Один із останніх переглянутих фільмів?", "");
-    let secondQuestion = +prompt("На скільки оціните його?", "");
-    personalMovieDB.movies[firstQuestion] = secondQuestion;
+ if (personalMovieDB.count < 10) {
+    alert("Ви переглянули досить мало фільмів");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert("Ви класичний глядач");
+} else if (personalMovieDB.count >= 30) {
+    alert("Ви кіноман");
+} else {
+    alert("Виникла помилка :-(");
 }
 
 
+ for(let i = 0; i < 2; i++) {
+
+    let firstQuestion = prompt("Один із останніх переглянутих фільмів?", "");
+    let secondQuestion = +prompt("На скільки оціните його?", "");
+
+    if (firstQuestion != null && secondQuestion != null && firstQuestion != '' && secondQuestion != '' && firstQuestion.length < 50) {
+        personalMovieDB.movies[firstQuestion] = secondQuestion;
+    } else { 
+        i--;
+    }
+
+};
+
 console.log(personalMovieDB);
+
