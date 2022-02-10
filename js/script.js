82ccff2fc956474ed2c8c5152ac94c47ec8cf5ad
Yuliya <ypolis@softserveinc.com>
2022-02-10 22:51:14 +0200

    dded methods to object

--- js/script.js
+++ js/script.js
@@ -1,76 +1,70 @@
 'use strict';
 
-let numberOfFilms;
-
-function start() {
-
-    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
-        numberOfFilms = +prompt("Скільки фільмів ви уже подивились?", "");
-        console.log(numberOfFilms);
-    }
-}
-
-start();
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-
-};
-
-detectPersonalLevel();
-
-function rememberMyFilms() {
-    for(let i = 0; i < 2; i++) {
-
-        let firstQuestion = prompt("Один із останніх переглянутих фільмів?", "");
-        let secondQuestion = +prompt("На скільки оціните його?", "");
-    
-        if (firstQuestion != null && secondQuestion != null && firstQuestion != '' && secondQuestion != '' && firstQuestion.length < 50) {
-            personalMovieDB.movies[firstQuestion] = secondQuestion;
-        } else { 
-            i--;
+    privat: false,
+    start: function() {
+        while(personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)){
+            personalMovieDB.count = +prompt("Скільки фільмів ви уже подивились?", "");
+    }
+    },
+    showMyDB: function() {
+        if(personalMovieDB.privat == false){
+            console.log(personalMovieDB);
+        } else {
+            console.log("Sorry, the db is privat");
         }
+    },
+    toggleVisibleMyDB: function() {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
+    rememberMyFilms: function() {
+        for(let i = 0; i < 2; i++) {
     
-    };
-}
-
-rememberMyFilms();
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        alert("Ви переглянули досить мало фільмів");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        alert("Ви класичний глядач");
-    } else if (personalMovieDB.count >= 30) {
-        alert("Ви кіноман");
-    } else {
-        alert("Виникла помилка :-(");
-    }
-    
+            let firstQuestion = prompt("Один із останніх переглянутих фільмів?", "");
+            let secondQuestion = +prompt("На скільки оціните його?", "");
+        
+            if (firstQuestion != null && secondQuestion != null && firstQuestion != '' && secondQuestion != '' && firstQuestion.length < 50 && secondQuestion != NaN) {
+                personalMovieDB.movies[firstQuestion] = secondQuestion;
+            } else { 
+                i--;
+            }
+        
+        };
+    },
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            alert("Ви переглянули досить мало фільмів");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            alert("Ви класичний глядач");
+        } else if (personalMovieDB.count >= 30) {
+            alert("Ви кіноман");
+        } else {
+            alert("Виникла помилка :-(");
+        }
+        
+    },
+    writeYourGenres: function() {
+   
+        for (let i = 1; i <= 3; i++) {
+            personalMovieDB.genres[i - 1] = prompt(`Ваш улюблений жанр під номером ${i}`);
+        }
+    } 
 };
 
-function showMyDB() {
-    if(personalMovieDB.privat == false){
-        console.log(personalMovieDB);
-    } else {
-        console.log("Sorry, the db is privat");
-    }
-}
+personalMovieDB.start();
+personalMovieDB.detectPersonalLevel();
+personalMovieDB.rememberMyFilms();
+personalMovieDB.writeYourGenres();
+personalMovieDB.showMyDB();
 
-showMyDB();
 
-function writeYourGenres() {
-   
-    for (let i = 1; i <= 3; i++) {
-        personalMovieDB.genres[i - 1] = prompt(`Ваш улюблений жанр під номером ${i}`);
-    }
-}
 
-writeYourGenres();
-
-console.log(personalMovieDB);
 
